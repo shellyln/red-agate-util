ed15ca000bb9a3fa30a22100cddd1117152dec5c
shellyln <shellyln@users.noreply.github.com>
2017-12-03 15:03:45 +0900

    v0.0.2

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// Copyright (c) 2017, Shellyl_N and Authors
+// license: ISC
+// https://github.com/shellyln
+
+
+export * from './convert/Base64';
+export * from './convert/Escape';
+export * from './convert/QueryString';
+export * from './convert/TextEncoding';
+export * from './convert/WordWrap';
+
+export * from './io/FileFetcher';
+export * from './io/FileSaver';
+export * from './io/Html5FileFetcher';
+export * from './io/Logger';
+export * from './io/Uri';
+
